12e3e3c128aaaee6decc302f5c1584123e24fd44
Zhicheng Wang <zcwang@thoughtworks.com>
2018-10-09 17:12:30 +0800

    chore: 向 github 结构看齐

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,6 +1,6 @@
 import { parseFolder } from './applet-builder';
 import * as fs from 'fs';
 
-const app1 = parseFolder('../applet1/build', 'applet1');
-const app2 = parseFolder('../applet2/build', 'applet2');
+const app1 = parseFolder('../react-applet-1/build', 'applet1');
+const app2 = parseFolder('../react-applet-2/build', 'applet2');
 fs.writeFileSync('./src/app/applet-builder/applets.json', JSON.stringify([app1, app2], null, 2));
