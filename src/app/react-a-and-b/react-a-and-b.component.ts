f0698b08b82769bcb017041d7a7a024c663a7ad5
Zhicheng Wang <zcwang@thoughtworks.com>
2018-10-09 14:37:54 +0800

    feat: 尝试在同一个页面加载两个 react 程序

--- src/app/react-a-and-b/react-a-and-b.component.ts
+++ src/app/react-a-and-b/react-a-and-b.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppletModel } from '../shared/applet-loader/applet.model';
 
 @Component({
   selector: 'app-react-a-and-b',
@@ -9,6 +10,33 @@ export class ReactAAndBComponent implements OnInit {
 
   constructor() {
   }
+
+  applets: AppletModel[] = [
+    {
+      id: 'react-a',
+      body: `<noscript>You need to enable JavaScript to run this app.</noscript><h3>Test React A</h3><div id="root_$MFID$"></div>`,
+      scripts: [
+        '/assets/applet1/static/js/loader.js',
+        '/assets/applet1/static/js/1.6105a37c.chunk.js',
+        '/assets/applet1/static/js/main.fca4e889.chunk.js',
+      ],
+      styles: [
+        '/assets/applet1/static/css/main.4a4a01ed.chunk.css',
+      ],
+    },
+    {
+      id: 'react-b',
+      body: `<noscript>You need to enable JavaScript to run this app.</noscript><h3>Test React B</h3><div id="root_$MFID$"></div>`,
+      scripts: [
+        '/assets/applet2/static/js/loader.js',
+        '/assets/applet2/static/js/1.6105a37c.chunk.js',
+        '/assets/applet2/static/js/main.a1469a93.chunk.js',
+      ],
+      styles: [
+        '/assets/applet2/static/css/main.7a0cc0c9.chunk.css',
+      ],
+    },
+  ];
 
   ngOnInit() {
   }
